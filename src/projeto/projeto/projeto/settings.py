3169b770c43626ae9b10cd093460d94788e3030d
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2020-07-01 12:55:58 -0300

    Liga as aplicações ao projeto

--- src/projeto/projeto/projeto/settings.py
+++ src/projeto/projeto/projeto/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SalaDeAula',
+    'Disciplina',
+    'Professor',
+    'Turma',
+    'SlotDeHorario',
 ]
 
 MIDDLEWARE = [
